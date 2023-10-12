4eb210930be042d5a6e29e2ad95af6fd9c792b12
LukeHollingsworth <lukehollingsworth99@hotmail.com>
2023-10-12 20:08:51 +0100

    bug in page name fixed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const bigG = document.querySelector(".GText");
-    const pages = ["trueSect/trueSect.html", "newSect/newSect.html", "lore.html", "calendar.html"];
+    const pages = ["truesect/truesect.html", "newsect/newsect.html", "lore.html", "calendar.html"];
   
     bigG.addEventListener("click", function() {
       const randomPage = pages[Math.floor(Math.random() * pages.length)];
